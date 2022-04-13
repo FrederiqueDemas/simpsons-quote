c5683f58ae228eeed320271a016a9ff4a474b6d7
FREDERIQUE DEMAS <frederique.demas73@gmail.com>
2022-04-13 19:27:29 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import QuoteCard from "./QuoteCard";
 
 function App() {
+  const simpsonCard =
+      {
+      quote:
+        "These are my only friends...grown-up nerds like Gore Vidal. And even he's kissed more boys than I ever will.",
+      character: "Lisa Simpson",
+      image:
+        "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083",
+      characterDirection: "Right",
+    }
+  const [quoteList, setQuoteList] = React.useState(simpsonCard)
+  
+  const getQuote = () => {
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+      .then((response) => response.data)
+      .then((data) => {
+        console.log(data)
+        setQuoteList(data[0]);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <QuoteCard quoteList={quoteList} />
+      <button type="button" onClick={getQuote}>
+        Simpson quote
+      </button>
     </div>
   );
 }
---
+++ src/QuoteCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './QuoteCard.css';
+
+const QuoteCard = ({quoteList}) => (
+  <figure className="QuoteCard">
+    <img src={quoteList.image} alt={quoteList.character} />
+    <figcaption>
+      <blockquote>{quoteList.quote}</blockquote>
+      <cite>{quoteList.character}</cite>
+    </figcaption>
+  </figure>
+);
+
+export default QuoteCard;
